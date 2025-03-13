0d55a1fc860140db4927ea6bc087b7afa523fede
MTJonathan <jonathan02330@gmail.com>
2025-03-13 14:10:25 -0500

    LocalStorage Idiomas

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,12 +1,24 @@
 import '../styles/nav.css'
+import { useEffect, useState } from 'react';
 import { useTranslation } from "react-i18next";
 import { Flecha, Logo } from './svg/svg'
 
 const Nav = () => {
+  const [idioma, setIdioma] = useState("es");
   const {t, i18n} = useTranslation("nav");
+  
+  useEffect(() => {
+    const idiomaGuardar = localStorage.getItem("idioma");
+    if (idiomaGuardar) {
+      i18n.changeLanguage(idiomaGuardar);
+      setIdioma(idiomaGuardar);
+    }
+  }, [])
+  
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
-    console.log(lng)
+    setIdioma(lng);
+    localStorage.setItem("idioma", lng);
   }
   return (
     <nav className='nav'>
@@ -17,7 +29,7 @@ const Nav = () => {
         <li><a href="">{t('resources')}</a></li>
         <li><a href="">{t('pricing')}</a></li>
         <li>
-          <select onChange={(e) => changeLanguage(e.target.value)}>
+          <select value={idioma} onChange={(e) => changeLanguage(e.target.value)}>
             <option value={"es"}>Español</option>
             <option value={"en"}>English</option>
           </select>
