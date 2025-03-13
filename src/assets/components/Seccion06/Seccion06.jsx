f6a2954b765409c69655542e2fab8550d0e4a09f
MTJonathan <jonathan02330@gmail.com>
2025-03-13 15:20:19 -0500

    Idiomas S6

--- src/assets/components/Seccion06/Seccion06.jsx
+++ src/assets/components/Seccion06/Seccion06.jsx
@@ -1,27 +1,26 @@
 import "./Seccion06.css";
 import "./animaciones.css"
+import { useTranslation } from "react-i18next";
 import { Flecha, Phone } from "../svg/svg";
 
 const Seccion06 = () => {
+  const { t } = useTranslation("seccion06");
   return (
     <section className="seccion06 container">
       <div className="s6Container">
         <header>
-          <h2>Toma el control de tu productividad con chatbot GPT-4.</h2>
+          <h2>{t("title")}</h2>
         </header>
         <div className="s6txt">
           <div className="txtS6">
-            <p>
-              Transforma tu servicio al cliente con la IA de Rotar y el chatbot
-              impulsado por GPT-4. Pruébalo gratis ahora.
-            </p>
+            <p>{t("description")}</p>
           </div>
           <div className="btnS6">
             <a href="#">
-              Reserva una demo <Flecha />
+              {t("btnDemo")} <Flecha />
             </a>
             <a href="#">
-              Contáctanos <Phone />
+              {t("btnContact")} <Phone />
             </a>
           </div>
         </div>
